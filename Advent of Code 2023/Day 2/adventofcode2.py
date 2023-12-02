cfe7ac9a7c4c32f60ae7fa425bcd15b502707a40
qtbodart <quentin.bodart@student.uclouvain.be>
2023-12-02 10:52:17 +0100

    elNumerator function fixed

--- Advent of Code 2023/Day 2/adventofcode2.py
+++ Advent of Code 2023/Day 2/adventofcode2.py
@@ -17,10 +17,10 @@ def elNumerator(line):
         sub_colors = subset.split(',')
         for sub_color in sub_colors:
             info = sub_color.strip().split(' ')
-            num = info[0]
+            num = int(info[0])
             col = info[1]
-            print(num)
-            if output[colors.index(col)] < int(num) :
-                output[colors.index(col)] == int(num)
+            min = output[colors.index(col)]
+            if min < num:
+                output[colors.index(col)] = num
     return output
 print(elNumerator('Game 2: 6 blue, 3 green; 4 red, 1 green, 7 blue; 2 green'))
