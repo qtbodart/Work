f2543355665361241189312b1e374895d61afae0
qtbodart <quentin.bodart@student.uclouvain.be>
2023-12-05 13:41:02 +0100

    intervalModifier functions as intended

--- Advent of Code 2023/Day 5/adventofcode5_2.py
+++ Advent of Code 2023/Day 5/adventofcode5_2.py
@@ -1,4 +1,17 @@
 numbers = ['0','1','2','3','4','5','6','7','8','9']
+lines = open('Day 5/test.txt').readlines()
+
+def tableConvertor(lines):
+    matrix = []
+    table = []
+    for line in lines:
+        if line[0] not in numbers:
+            if line == '\n':
+                matrix.append(table)
+                table = []
+            continue
+        table.append([int(number) for number in line.split(' ')])
+    return matrix
 
 def seedisator(lines):
     treated = lines[0].split(':')[1].split(' ')
@@ -13,3 +26,30 @@ def maxOfSeeds(lines):
             max = pot_max
     return max
 
+interval = [([0,maxOfSeeds(lines)*10],0)]
+print(interval)
+
+def intervalModifier(fr, to, modifier):
+    for i in range(len(interval)):
+        rng, cur_modifier = interval[i]
+        fr_int, to_int = rng
+        if fr_int < fr and to < to_int:
+            interval.remove(([fr_int,to_int], cur_modifier))
+            interval.append(([fr_int,fr], cur_modifier))
+            interval.append(([to, to_int], cur_modifier))
+            interval.append(([fr,to], cur_modifier+modifier))
+            continue
+        if fr_int >= fr and to > fr_int and to < to_int:
+            interval.remove(([fr_int,to_int], cur_modifier))
+            interval.append(([fr_int, to], cur_modifier+modifier))
+            interval.append(([to, to_int], cur_modifier))
+            continue
+        if fr_int < fr and fr < to_int and to_int <= to:
+            print(to_int)
+            print(to)
+            interval.remove(([fr_int,to_int], cur_modifier))
+            interval.append(([fr_int,fr], cur_modifier))
+            interval.append(([fr, to_int], cur_modifier+modifier))
+            continue
+        elif (fr<fr_int & to<fr_int) or (to_int<fr & to_int<to):
+            continue
