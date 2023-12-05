a96224486e2c75101963f9be1d1c53880f531b24
qtbodart <quentin.bodart@student.uclouvain.be>
2023-12-05 09:41:49 +0100

    Test passed for part1

--- Advent of Code 2023/Day 5/adventofcode5.py
+++ Advent of Code 2023/Day 5/adventofcode5.py
@@ -13,11 +13,34 @@ def tableConvertor(lines):
                 matrix.append(table)
                 table = []
             continue
-        
         table.append([int(number) for number in line.split(' ')])
-    
     return matrix
 
+def destinationConvertor(matrix, seed):
+    current_value = seed
+    print(f'\nseed considered : {seed}\n-------------\n ')
+    for table in matrix:
+        for line in table:
+            if (line[1]<=current_value & current_value<line[1]+line[2]):
+                print(f'line : {line}\nvalue found to be between {line[1]} and {line[1]+line[2]}\nold value : {current_value}  ')
+                current_value = current_value+(line[0]-line[1])
+                print(f'new value : {current_value}\n')
+                break
+    return current_value
+
+def elGrandeExecutor(lines):
+    seeds = seedisator(lines)
+    destinations = []
+    matrix = tableConvertor(lines)
+
+    for seed in seeds:
+        destination = destinationConvertor(matrix,seed)
+        destinations.append(destination)
+    
+    return destinations
+
+        
+
 if __name__ == '__main__':
-    lines = open('Day 5/input5.txt').readlines()
-    print(tableConvertor(lines))
+    lines = open('Day 5/test.txt').readlines()
+    print(elGrandeExecutor(lines))
