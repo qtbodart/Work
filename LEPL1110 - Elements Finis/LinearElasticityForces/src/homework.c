9830d9a7484da16b7b38ab9af27fc252a334cdd5
qtbodart <quentin.bodart@student.uclouvain.be>
2024-03-30 13:50:49 +0100

    version Issambre

--- LEPL1110 - Elements Finis/LinearElasticityForces/src/homework.c
+++ LEPL1110 - Elements Finis/LinearElasticityForces/src/homework.c
@@ -86,25 +86,135 @@ void femElasticityAssembleNeumann(femProblem *theProblem){
         // A completer :-)   
         //
 
+         if (type == NEUMANN_X || type == NEUMANN_Y) {
+            for (i = 0; i < theCondition->domain->nElem; i++) {
+                iElem = theCondition->domain->elem[i];
+
+                for (j = 0; j < nLocal; j++) {
+                    map[j] = theEdges->elem[2 * iElem + j];
+                    x[j] = theNodes->X[map[j]];
+                    y[j] = theNodes->Y[map[j]];
+                }                
+
+                double xsi[2] = {-1/sqrt(3), 1/sqrt(3)};
+                double phi0[2] = {(1 - xsi[0])/2, (1 - xsi[1])/2};
+                double phi1[2] = {(1 + xsi[0])/2, (1 + xsi[1])/2};
+
+                double integral[2] = {0, 0};
+
+                for (int i = 0; i < 2; i++) {
+                    integral[0] += phi0[i]*sqrt((x[1] - x[0])*(x[1] - x[0]) + (y[1] - y[0])*(y[1] - y[0]));
+                    integral[1] += phi1[i]*sqrt((x[1] - x[0])*(x[1] - x[0]) + (y[1] - y[0])*(y[1] - y[0]));
+                }
+
+                if (type == NEUMANN_X) {
+                    B[2 * map[0]] += integral[0] * value;
+                    B[2 * map[1]] += integral[1] * value;
+                } else {
+                    B[2 * map[0] + 1] += integral[0] * value;
+                    B[2 * map[1] + 1] += integral[1] * value;
+                }
+            }
+        }
     }
 }
 
 
 
 double *femElasticitySolve(femProblem *theProblem){
- 
-    //       
-    // A completer :-) 
-    //  
 
-     return theProblem->soluce;
+    femFullSystem  *theSystem = theProblem->system;
+    femIntegration *theRule = theProblem->rule;
+    femDiscrete    *theSpace = theProblem->space;
+    femGeo         *theGeometry = theProblem->geometry;
+    femNodes       *theNodes = theGeometry->theNodes;
+    femMesh        *theMesh = theGeometry->theElements;
+    
+    
+    double x[4],y[4],phi[4],dphidxsi[4],dphideta[4],dphidx[4],dphidy[4];
+    int iElem,iInteg,iEdge,i,j,d,map[4],mapX[4],mapY[4];
+    
+    int nLocal = theMesh->nLocalNode;
+
+    double a   = theProblem->A;
+    double b   = theProblem->B;
+    double c   = theProblem->C;      
+    double rho = theProblem->rho;
+    double g   = theProblem->g;
+    double **A = theSystem->A;
+    double *B  = theSystem->B;
+
+    femElasticityAssembleElements(theProblem);
+    femElasticityAssembleNeumann(theProblem);
+
+    for (iElem = 0; iElem < theMesh->nElem; iElem++) {
+        for (j = 0; j < nLocal; j++) {
+            map[j] = theMesh->elem[iElem * nLocal + j];
+            mapX[j] = 2 * map[j];
+            mapY[j] = 2 * map[j] + 1;
+            x[j] = theNodes->X[map[j]];
+            y[j] = theNodes->Y[map[j]];
+        }
+
+        for (int iInteg = 0; iInteg < theRule->n; iInteg++) {
+            femDiscretePhi2(theSpace, theRule->xsi[iInteg], theRule->eta[iInteg], phi);
+            femDiscreteDphi2(theSpace, theRule->xsi[iInteg], theRule->eta[iInteg], dphidxsi, dphideta);
+
+            double dxdxsi = 0.0;
+            double dxdeta = 0.0;
+            double dydxsi = 0.0;
+            double dydeta = 0.0;
+            for (i = 0; i < theSpace->n; i++) {
+                dxdxsi += x[i] * dphidxsi[i];
+                dxdeta += x[i] * dphideta[i];
+                dydxsi += y[i] * dphidxsi[i];
+                dydeta += y[i] * dphideta[i];
+            }
+            double HitTheRoadJac = fabs(dxdxsi * dydeta - dxdeta * dydxsi);
+
+            for (i = 0; i < theSpace->n; i++) {
+                dphidx[i] = (dphidxsi[i] * dydeta - dphideta[i] * dydxsi) / HitTheRoadJac;
+                dphidy[i] = (dphideta[i] * dxdxsi - dphidxsi[i] * dxdeta) / HitTheRoadJac;
+            }
+            for (i = 0; i < theSpace->n; i++) {
+                for (j = 0; j < theSpace->n; j++) {
+                    A[mapX[i]][mapX[j]] += (dphidx[i] * dphidx[j] * a + dphidy[i] * dphidy[j] * c) * HitTheRoadJac * theRule->weight[iInteg];
+                    A[mapY[i]][mapY[j]] += (dphidy[i] * dphidy[j] * a + dphidx[i] * dphidx[j] * c) * HitTheRoadJac * theRule->weight[iInteg];
+                    A[mapX[i]][mapY[j]] += (dphidx[i] * dphidy[j] * b + dphidy[i] * dphidx[j] * c) * HitTheRoadJac * theRule->weight[iInteg];
+                    A[mapY[i]][mapX[j]] += (dphidy[i] * dphidx[j] * b + dphidx[i] * dphidy[j] * c) * HitTheRoadJac * theRule->weight[iInteg];
+                }
+            }
+            for (i = 0; i < theSpace->n; i++) {
+                B[mapY[i]] -= phi[i] * g * rho * HitTheRoadJac * theRule->weight[iInteg];
+            }
+        }
+    }
+  
+    int *theConstrainedNodes = theProblem->constrainedNodes;     
+    for (int i=0; i < theSystem->size; i++) {
+        if (theConstrainedNodes[i] != -1) {
+            double value = theProblem->conditions[theConstrainedNodes[i]]->value;
+            femFullSystemConstrain(theSystem,i,value); }}
+                            
+    return femFullSystemEliminate(theSystem); 
 }
 
 double * femElasticityForces(femProblem *theProblem){        
            
-    //       
-    // A completer :-) 
-    //  
+    double *solution = theProblem->soluce;
+
+    femFullSystem *theSystem = theProblem->system;
+    double **A = theSystem->A;
+    double *B = theSystem->B;
+    int size = theSystem->size;
+
+    for (int i = 0; i < size; i++) {
+        double internalForce = 0.0;
+        for (int j = 0; j < size; j++) {
+            internalForce += A[i][j] * solution[j];
+        }
+        theProblem->residuals[i] = B[i] - internalForce;
+    }
 
     return theProblem->residuals;
 }
