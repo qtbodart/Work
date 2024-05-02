ed3eb855633bbe3a19ffdf13f1050534aa55684f
qtbodart <quentin.bodart@student.uclouvain.be>
2024-05-02 16:29:46 +0200

    plot finished

--- LEPL1505 - Projet 4 en chimie et physique/Résultats d'expériences/plot.py
+++ LEPL1505 - Projet 4 en chimie et physique/Résultats d'expériences/plot.py
@@ -1,10 +1,63 @@
 import matplotlib.pyplot as plt
+import csv
 
-def pltFTIR():
-    pass
+# unset GTK_PATH
 
-def pltTGA():
-    pass
+def parseFTIR(filename):
+    wn = [] # wave number (cm-1)
+    t = [] # transmittance
+
+    with open(filename) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            wn.append(float(row[0][0:7])*(10**int(row[0][-3:])))
+            t.append(float(row[1][0:7])*(10**int(row[1][-3:])))
+    
+    return wn, t
+
+def pltFTIR(filename):
+    wn, t = parseFTIR(filename)
+
+    plt.title("FTIR - " + filename.split("/")[1].split(".")[0])
+    plt.xlabel("Wave number [cm-1]")
+    plt.ylabel("Transmittance")
+    plt.plot(wn, t)
+    ax = plt.axis()
+    plt.axis((ax[1],ax[0],ax[2],ax[3]))
+    plt.show()
+    
+def parseTGA(filename):
+    t = []   # temperature
+    w = []  # weight
+
+    with open(filename) as f:
+        for line in f.readlines():
+            aline = [value for value in line.split(" ") if value != ""]
+            aline[-1] = aline[-1][:-1]
+            
+            if aline[0].isdigit(): # checking if line contains informations
+                # weight
+                if aline[2].startswith("-"):
+                    w.append(-float(aline[2][1:8].replace(",","."))*(10**(int(aline[2][9:]))))
+                else:
+                    w.append(float(aline[2][:7].replace(",","."))*(10**(int(aline[2][8:]))))
+
+                # temperature
+                if aline[3].startswith("-"):
+                    t.append(-float(aline[3][1:8].replace(",","."))*(10**(int(aline[3][9:]))))
+                else:
+                    t.append(float(aline[3][:7].replace(",","."))*(10**(int(aline[3][8:]))))
+    
+    return t, w
+
+def pltTGA(filename):
+    t, w = parseTGA(filename)
+
+    plt.title("TGA - " + filename.split("/")[1].split(".")[0])
+    plt.xlabel("Temperature [°C]")
+    plt.ylabel("Mass [g]")
+    plt.plot(t, w)
+    plt.show()
 
 def parseDSC(filename):
     t = []   # temperature
@@ -29,13 +82,24 @@ def parseDSC(filename):
                     t.append(float(aline[3][:7].replace(",","."))*(10**(int(aline[3][8:]))))
     
     return t, hf
-        
-
 
 def pltDSC(filename):
     t, hf = parseDSC(filename)
 
+    plt.title("DSC - " + filename.split("/")[1].split(".")[0])
+    plt.xlabel("Temperature [°C]")
+    plt.ylabel("Heatflow [W/g]")
     plt.plot(t, hf)
     plt.show()
 
-pltDSC("DSC/1e chauffe.txt")
+# Change "/" into "\\" on Windows 
+def pltAll():
+    pltDSC("DSC/1e chauffe.txt")
+    pltDSC("DSC/2e chauffe.txt")
+    pltDSC("DSC/Refroidissement.txt")
+    pltFTIR("FTIR - ATR/Boite noire t.CSV")
+    pltTGA("TGA/TGA.txt")
+
+
+
+pltAll()
